6dade258dc29123956dad0fbd57431e154c34f98
Steven Farkas <sfarkas@sjfcg.com>
2013-02-01 10:47:27 -0800

    this is my first commit to git through rubymine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '48b002e31acdef30ae183e35a783f5de42477e2e94e9e8cc5f118ace40e22a927d98188249742035d2fcef54624a633048dbdb1861b297e666adba9e645edfc7'
